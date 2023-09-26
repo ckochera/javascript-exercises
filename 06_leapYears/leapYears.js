f8c06f9099c3999ecd58d0a5c16ffaf6f4dc9502
Christopher Kochera <christopherkochera@ChristophersMBP.lan>
2023-09-26 16:35:23 -0400

    Complete exercise 6
    Completed leap years exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = (year) => {
+    if (!Number.isInteger(year)) {
+        return "ERROR";
+    } else if (year % 4 === 0 && year % 100 != 0) {
+    return true;
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-};
+leapYears(1997);
+
 
 // Do not edit below this line
 module.exports = leapYears;
