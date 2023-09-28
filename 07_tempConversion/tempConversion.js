b472967499e070357dee8ecf94c985e17922b275
Christopher Kochera <christopherkochera@ChristophersMBP.lan>
2023-09-28 14:17:14 -0400

    Complete exercise 7
    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+// function to convert degrees Fahrenheit to Celsius
+
+const convertToCelsius = (degreesF) => {
+    let celsiusFormula = ((degreesF - 32) * 0.5556);
+    let celsiusTemp = Math.round(celsiusFormula * 10) / 10;
+    return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(-100);
+
+// function to convert degrees Celsius to Fahrenheit
+
+const convertToFahrenheit = function(degreesC) {
+  let fahrenheitFormula = ((degreesC * 1.8) + 32);
+  let fahrenheitTemp = Math.round(fahrenheitFormula * 10) / 10;
+  return fahrenheitTemp;
 };
+
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
